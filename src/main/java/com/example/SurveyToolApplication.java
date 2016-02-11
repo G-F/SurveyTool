e94a9131ef8f0c4db799439f0bc8bb83dabec7fc
Masao Kanamori <golden.foresst627pgmail.com>
2016-02-11 23:00:22 +0900

    プロジェクトの初期化

---
+++ src/main/java/com/example/SurveyToolApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyToolApplication.class, args);
+	}
+}
