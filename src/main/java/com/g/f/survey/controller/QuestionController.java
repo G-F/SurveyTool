685f028d6f0ece18e6a5efe5d85a3f739dabdb42
Masao Kanamori <golden.foresst627pgmail.com>
2016-02-17 00:10:40 +0900

    ログの設定を追加

--- src/main/java/com/g/f/survey/controller/QuestionController.java
+++ src/main/java/com/g/f/survey/controller/QuestionController.java
@@ -2,7 +2,10 @@ package com.g.f.survey.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,23 +20,26 @@ public class QuestionController {
 
 	@Autowired
 	QuestionService questionService;
+
+	private static final Logger logger = LoggerFactory.getLogger(QuestionController.class);
 	
 	@RequestMapping(method = RequestMethod.GET)
 	List<Question> getQuestions() {
 		return questionService.findAll();
 	}
 	
-	Question insertQuestion(@RequestBody Question question) {
+	@RequestMapping(method = RequestMethod.POST)
+	Question insertQuestion(@Validated @RequestBody Question question) {
+		
+		logger.info("Start insertQuestion():" + question);
 		
 		// 自由筆記フラグの指定がない場合はfalseとして扱う
 		if(question.getFreeDescriptionFlag() == null) {
-			System.out.println("自由記述フラグがnull");
 			question.setFreeDescriptionFlag(false);
 		}
 		
 		// 回答上限数の指定がない場合、1に設定する
 		if(question.getUpperLimitAnswers() == null) {
-			System.out.println("回答数上限がnull");
 			question.setUpperLimitAnswers(1);
 		}
 		
